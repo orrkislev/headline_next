45b09f4bc84a352b88517f22adda0785e4cba0b4
Orr Kislev <orrkislev@gmail.com>
2025-03-02 17:16:35 +0200

    added last yesterday summary

--- app/[locale]/[country]/RightPanel.js
+++ app/[locale]/[country]/RightPanel.js
@@ -9,10 +9,16 @@ export default function RightPanel({ summaries, locale, date, setDate }) {
     const daySummaries = useMemo(() => {
         return summaries.filter(summary => summary.timestamp.toDateString() === day)
     }, [summaries, day])
+    const lastSummaryDayBefore = useMemo(() => {
+        const dayBefore = new Date(day)
+        dayBefore.setDate(dayBefore.getDate() - 1)
+        const dayBeforeString = dayBefore.toDateString()
+        return summaries.filter(summary => summary.timestamp.toDateString() === dayBeforeString)[0]
+    }, [summaries, day])
 
-    const activeSummary = useMemo(() => {
-        return daySummaries.find(summary => summary.timestamp <= date)
-    }, [daySummaries, date]);
+    const activeSummaryId = useMemo(() => {
+        return summaries.find(summary => summary.timestamp <= date)?.id
+    }, [summaries, date]);
 
     return (
         <div className={`summary-section flex flex-col gap-4 h-full overflow-hidden px-4 pb-2`}>
@@ -20,8 +26,11 @@ export default function RightPanel({ summaries, locale, date, setDate }) {
             {/* <DailySummary locale={locale} /> */}
             <div className={`custom-scrollbar h-full flex flex-col h-full p-2 ${locale === 'heb' ? 'pl-4' : 'pr-4'}`}>
                 {daySummaries.map((summary, i) => (
-                    <Summary key={i} summary={summary} active={summary.id === activeSummary.id} locale={locale} setDate={setDate} />
+                    <Summary key={i} summary={summary} active={summary.id === activeSummaryId} locale={locale} setDate={setDate} />
                 ))}
+                {lastSummaryDayBefore && (
+                    <Summary summary={lastSummaryDayBefore} active={lastSummaryDayBefore.id === activeSummaryId} locale={locale} setDate={setDate} />
+                )}
             </div>
             <div className='py-2 bg-white border-t border-gray-200'>
                 {/* <YesterdaySummaryTitle lastSummaryDayBefore={lastSummaryDayBefore} /> */}
