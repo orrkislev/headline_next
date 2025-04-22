da941cd38100434fe1ff638cead928884606be1e
SFKislev <31474718+SFKislev@users.noreply.github.com>
2025-04-22 14:36:09 +0300

    fixed padding issues

--- app/[locale]/[country]/Source/Headine.js
+++ app/[locale]/[country]/Source/Headine.js
@@ -11,7 +11,7 @@ export default function Headline({ headline, typography, loading }) {
                 <div className={`animate-headline w-full text-lg font-semibold break-words line-clamp-6`}    
                     style={{ ...typography, width: '100%', direction: isRTL ? 'rtl' : 'ltr' }} key={headline.id}>
                     {loading && (
-                        <span className="inline-block mr-2 ml-2">
+                        <span className="inline-flex items-center mx-3">
                             <div className="w-5 h-5 border-2 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
                         </span>
                     )}
