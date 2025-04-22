445bb110b7f6d0e9c701b4b0d914705fb513e1f4
SFKislev <31474718+SFKislev@users.noreply.github.com>
2025-04-22 09:12:26 +0300

    circular loader in the headline during loading state

--- app/[locale]/[country]/Source/Headine.js
+++ app/[locale]/[country]/Source/Headine.js
@@ -1,15 +1,22 @@
 import { checkRTL } from "@/utils/utils";
-import { Skeleton } from "@mui/material";
 
 export default function Headline({ headline, typography, loading }) {
-    if (!headline.headline || headline.headline == '') return <Skeleton variant="text" width={200} />;
+    if (!headline.headline || headline.headline == '') return null;
     const txt = headline.headline;
     const isRTL = checkRTL(txt);
+    
     return (
         <a href={headline.link} target="_blank" rel="noopener noreferrer">
-            <div className={`animate-headline w-full text-lg font-semibold break-words line-clamp-6 ${loading ? 'animate-slow-fade' : ''}`}    
-                style={{ ...typography, width: '100%', direction: isRTL ? 'rtl' : 'ltr' }} key={headline.id}>
-                {txt}
+            <div className="relative">
+                <div className={`animate-headline w-full text-lg font-semibold break-words line-clamp-6`}    
+                    style={{ ...typography, width: '100%', direction: isRTL ? 'rtl' : 'ltr' }} key={headline.id}>
+                    {loading && (
+                        <span className="inline-block mr-2">
+                            <div className="w-5 h-5 border-2 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
+                        </span>
+                    )}
+                    {txt}
+                </div>
             </div>
         </a>
     );
